e2b0ada538504d48d68d9507e79f01d4f7ffdc19
ptypecolorss <118310925+ptypecolorss@users.noreply.github.com>
2023-02-12 15:24:18 +0300

    Delete js directory

--- js/holo.js
+++
@@ -1,9 +0,0 @@
-pic = new Array(
-    '<img src="hg/1.gif" style="max-width:500px" alt="Талисман Сочи-2014">',
-    '<img src="hg/2.gif" style="max-width:500px" alt="Gawr Gura">',
-    '<img src="hg/3.gif" style="max-width:500px" alt="Gawr Gura">',
-    '<img src="hg/4.gif" style="max-width:500px" alt="Gawr Gura">',
-    '<img src="hg/5.gif" style="max-width:500px" alt="Gawr Gura">',
-);
-n = Math.floor(Math.random() * 5);
-document.write(pic[n]);
